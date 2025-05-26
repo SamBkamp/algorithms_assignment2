fc5ae1556bbb48cbc7f93677c18f0fccd26bf80f
Sam Bonnekamp <s.bonnekamp@exotic-aqua.com>
2025-05-26 19:20:06 +0800

    updated generator script

--- genList.py
+++ genList.py
@@ -1,9 +1,15 @@
 #!/bin/python3
 
 import random
+import sys
 
 amount = 1000
-print("#define IN_LEN " + str(amount))
 
-for i in range(0, amount):
-    print("list["+str(i)+"] = "+str(random.randint(0, 800))+";")
+if(len(sys.argv) < 2):
+    print("usage: "+sys.argv[0]+" random")
+    exit(0)
+
+if sys.argv[1] == "random":
+    print("#define IN_LEN " + str(amount))
+    for i in range(0, amount):
+        print("list["+str(i)+"] = "+str(random.randint(0, 800))+";")
